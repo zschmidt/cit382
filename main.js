90d83341fcaf37d07b1907087b257c1138d93c0e
Zach Schmidt <zschmidt@uoregon.edu>
2016-02-23 10:24:58 -0800

    Added main.js

--- main.js
+++ main.js
@@ -2,7 +2,27 @@ var http = require('http');
 
 function main(){
 	var url = "http://api.openweathermap.org/data/2.5/weather?zip=";
-	var zip = "97403";
+	var zip;
+
+	//This nifty bit of code allows us to read command line arguments
+	/*
+	process.argv.forEach(function (val, index, array) {
+		console.log(index + ': ' + val);
+	});
+	*/
+
+	if(process.argv.length < 3)
+	{
+		console.log("Hint: You can provide a zip code as a command line argument");
+		console.log("\t(i.e. 'node main.js 97403')");
+		console.log("\t--Using default zip code--");
+		zip = "97403";
+	}
+	else
+	{
+		zip = process.argv[2];
+	}
+	
 	url+=zip;
 	//this is just some id which allows us to query the API
 	url+=",us&appid=44db6a862fba0b067b1930da0d769e98";
@@ -16,10 +36,9 @@ function main(){
 			data = JSON.parse(data);
 			var city = data.name;
 			var temp = (data.main.temp - 273.15)* 1.8000 + 32.00;
-			console.log("The temperature in "+city+" is "+temp.toFixed(2)+" degrees fahrenheit");
+			console.log(`\nThe temperature in ${city} is ${temp.toFixed(2)} degrees fahrenheit\n`);
 		});
 	});
 }
-
 
 main();
